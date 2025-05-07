1033bfdcd8dc5f4f142c7ac87ca51e0762644794
William Mian <william_mian@yahoo.com.br>
2025-05-07 14:34:51 -0300

    swagger

---
+++ desafio-nubank/src/main/java/br/com/wm/desafionubank/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package br.com.wm.desafionubank.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+	
+	@Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Desafio Nubank API")
+                .version("1.0")
+                .description("Documentação da API para gerenciamento de clientes e seus contatos."));
+    }
+
+}
